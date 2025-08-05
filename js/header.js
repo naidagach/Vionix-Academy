b731d26ab48a2cf9671ac590d2b7f7d14b88557e
Naida Gachabayova <naishka2005@gmail.com>
2025-08-05 13:56:01 +0400

    header commit

---
+++ js/header.js
@@ -0,0 +1,28 @@
+function togglePanel(elem) {
+    const panel = elem.nextElementSibling
+    const icon = elem.querySelector('i')
+
+    document.querySelectorAll('.panel').forEach(p => {
+        if (p !== panel) {
+            p.classList.remove('h-[735px]')
+            p.classList.add('h-0')
+        }
+    })
+
+    document.querySelectorAll('.navmenu i').forEach(i => {
+        i.classList.remove('fa-caret-up')
+        i.classList.add('fa-caret-down')
+    })
+
+    if (panel.classList.contains('h-0')) {
+        panel.classList.remove('h-0')
+        panel.classList.add('h-[735px]')
+        icon.classList.remove('fa-caret-down')
+        icon.classList.add('fa-caret-up')
+      } else {
+        panel.classList.remove('h-[735px]')
+        panel.classList.add('h-0')
+        icon.classList.remove('fa-caret-up')
+        icon.classList.add('fa-caret-down')
+      }
+}
