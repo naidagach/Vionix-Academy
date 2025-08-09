fd453444e7cb4da4a4fc680285340fb8e17aa2ab
Naida Gachabayova <naishka2005@gmail.com>
2025-08-10 00:32:49 +0400

    vionix

--- js/header.js
+++ js/header.js
@@ -1,32 +1,35 @@
-function togglePanel(elem) {
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
 
-    const panel = elem.nextElementSibling
-    const icon = elem.querySelector('i')
-
-    
-    document.querySelectorAll('.panel').forEach(p => {
-        if (p !== panel) {
-            p.classList.remove('h-[400px]')
-            p.classList.add('h-0')
-        }
-    })
-    
-    document.querySelectorAll('.navmenu i').forEach(i => {
-        i.classList.remove('fa-caret-up')
-        i.classList.add('fa-caret-down')
-    })
-
-    if (panel.classList.contains('h-0')) {
-        panel.classList.remove('h-0')
-        panel.classList.add('h-[400px]')
-        icon.classList.remove('fa-caret-down')
-        icon.classList.add('fa-caret-up')
-      } else {
-        panel.classList.remove('h-[400px]')
-        panel.classList.add('h-0')
-        icon.classList.remove('fa-caret-up')
-        icon.classList.add('fa-caret-down')
-      }
-
-      
+function openMenu() {
+  menu.style.maxHeight = menu.scrollHeight + "px";
 }
+
+function closeMenu() {
+  menu.style.maxHeight = "0";
+}
+
+function toggleMenu() {
+  if (menu.style.maxHeight && menu.style.maxHeight !== "0px") {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+}
+
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation(); 
+  toggleMenu();
+});
+
+document.querySelectorAll(".menu-link").forEach(link => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && e.target !== menuBtn) {
+    closeMenu();
+  }
+});
